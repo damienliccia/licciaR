60786bc1e2d3dbbc5f744427ae3fe1ded7dbc054
damienliccia <liccia.damien@gmail.com>
2018-06-19 01:07:02 +0200

    ajout d'une nouvelle fonction

---
+++ R/user_in_depth.R
@@ -0,0 +1,60 @@
+#' Average audience
+#'
+#' @param utilisateur User ID or screen name
+#' @param token Your Twitter token
+#' @return list followers
+#' @export
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+
+user_in_depth <- function(user,
+                          token = NULL,
+                          n_tweets = NULL,
+                          output = NULL) {
+
+  ctr <- as.numeric(length(user))
+
+  user_temp <- user[ctr]
+  user_info <- rtweet::lookup_users(user_temp, token = token)
+  user_timeline <- rtweet::get_timeline(user_info$user_id, token = token, n = n_tweets)
+
+  list_possible_outputs <- data_frame(outputs = c("top_users_rt",
+                                                  "words_most_used"),
+                                      n = c("1",
+                                            "2"))
+
+  list_possible_outputs <- list_possible_outputs %>%
+    dplyr::filter(stringr::str_detect(outputs, output))
+
+  if(list_possible_outputs$n == 1) {
+
+    # Top users rt
+    top_users_rt <- user_timeline %>%
+      dplyr::mutate(text = str_to_lower(text)) %>%
+      dplyr::filter(str_detect(text, "rt @") == T) %>%
+      dplyr::select(text) %>%
+      dplyr::mutate(users_most_rt = qdapRegex::rm_between(text, "rt @", ":", extract=TRUE)) %>%
+      dplyr::mutate(users_most_rt = as.character(users_most_rt)) %>%
+      dplyr::count(users_most_rt, sort = T) %>%
+      dplyr::mutate(screen_name = user_timeline$screen_name[1]) %>%
+      dplyr::rename(n_top_users_rt = n) %>%
+      dplyr::select(screen_name,
+                    users_most_rt,
+                    n_top_users_rt)
+  } else  if(list_possible_outputs$n == 2) {
+
+    # Words most used
+    words_most_used <- user_timeline %>%
+      dplyr::mutate(text = str_to_lower(text)) %>%
+      dplyr::filter(str_detect(text, "rt @") == F) %>%
+      edouaRd::wordfrequency(text) %>%
+      dplyr::mutate(screen_name = user_timeline$screen_name[1]) %>%
+      dplyr::rename(n_words = n) %>%
+      dplyr::select(screen_name,
+                    words,
+                    n_words)
+  } else {
+    print("erreur")
+  }
+}
